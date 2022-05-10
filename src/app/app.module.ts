fb27a50eb4d7315e6e25a8d52c216099a72170a3
zettacamp-virgiawan-kusuma <virgiawan.kusuma@zettacamp.pro>
2022-05-10 11:03:57 +0700

    [Phase 1: Login Page] Setup routing on login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
